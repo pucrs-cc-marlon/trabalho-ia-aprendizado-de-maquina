a7b99512b8909646ae24208a00d03a33e63b91f8
Marlon Baptista de Quadros <marlonbaptista@hotmail.com>
2017-11-17 18:23:17 -0200

    - Estruturar arquivos Signed-off-by: Marlon Baptista de Quadros <marlonbaptista@hotmail.com>

--- pre_processamento.py
+++ pre_processamento.py
@@ -5,30 +5,45 @@ import nltk
 import pickle
 
 from bs4 import BeautifulSoup
+from cogroo_interface import Cogroo
 
 TESTE_DIR = 'CORPUS/TESTE/'
 TREINO_DIR = 'CORPUS/TREINO/'
-ARQUIVOS = ['CORPUS ESPACO DO TRABALHADOR.txt', 'CORPUS ESPORTES.txt',
-            'CORPUS POLICIA.txt', 'CORPUS SEU PROBLEMA E NOSSO.txt']
+ARQUIVOS = ['CORPUS ESPACO DO TRABALHADOR.txt', ]
+# 'CORPUS ESPORTES.txt', 'CORPUS POLICIA.txt', 'CORPUS SEU PROBLEMA E NOSSO.txt']
 
 
-class LimparArquivosTexto():
-
+class LimparArquivosTexto:
     def __init__(self):
-        self.clean_text(ARQUIVOS)
+        self.generate_structured_files(ARQUIVOS)
+        cogroo = Cogroo.Instance()
 
-    def clean_text(self, arquivos):
+    def generate_structured_files(self, arquivos):
         for filename in arquivos:
             with open('CORPUS/{}'.format(filename), 'r') as arq:
-                texto = ' '.join(arq.readlines())
-                bes = BeautifulSoup(texto, "html.parser") # Removendo as marcações
-                texto = bes.get_text() # Remove as Tags do texto
-                texto = self.remove_emails(texto) # Remover os e-mails
-                texto = self.remove_stopwords(texto) # Remover as stopwords
-                texto = nltk.wordpunct_tokenize(texto) # Tokenizar as palavras
-                pos_texto = self.pos_annotation(texto) # Adição da notação POS (part of speech)
-                texto = nltk.Text(texto) # Texto nos padrões do NLTK
+                lines = arq.readlines()
+                texto = ' '.join(lines)
+                textos = self.split_texts(lines)
+                textos = self.clean_texts(textos)
+                # self.create_bag_of_words(texto=texto)
 
+    def split_texts(self, lines):
+        textos = {}
+        actual_text = None
+        for line in lines:
+            if line.find('TEXTO') == 0:
+                actual_text = line.replace('\n', '')
+                textos[actual_text] = []
+            else:
+                if actual_text is not None:
+                    textos[actual_text].append(line)
+        return textos
+
+    def clean_texts(self, textos):
+        for texto in textos:
+            t = textos[texto]
+
+        return textos
 
     def remove_emails(self, text):
         re_email = r'\S*@\S*\s?'
@@ -47,3 +62,19 @@ class LimparArquivosTexto():
         sentences = portuguese_sent_tokenizer.tokenize(text)
         tags = [tagger.tag(nltk.word_tokenize(sentence)) for sentence in sentences]
         return tags
+
+    def lemmatize(self, text):
+        pass
+
+    def create_bag_of_words(self, texto):
+        bes = BeautifulSoup(texto, "html.parser")  # Removendo as marcações
+        texto = bes.get_text()  # Remove as Tags do texto
+        texto = self.remove_emails(texto)  # Remover os e-mails
+        texto = self.remove_stopwords(texto)  # Remover as stopwords
+        texto = nltk.wordpunct_tokenize(texto)  # Tokenizar as palavras
+        # pos_texto = self.pos_annotation(texto)  # Adição da notação POS (part of speech)
+        texto = nltk.Text(texto)  # Texto nos padrões do NLTK
+
+
+if __name__ == "__main__":
+    clean_files = LimparArquivosTexto()
