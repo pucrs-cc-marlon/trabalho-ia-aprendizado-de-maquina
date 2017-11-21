2b2d743c2731d7ea6041df3da00a3b51d70160c3
Marlon Baptista de Quadros <marlonbaptista@hotmail.com>
2017-11-21 20:52:32 -0200

    - Implementação parcial do algoritmo de mineração de texto, tokenização e estruturação Signed-off-by: Marlon Baptista de Quadros <marlonbaptista@hotmail.com>

--- pre_processamento.py
+++ pre_processamento.py
@@ -2,6 +2,8 @@
 
 import re
 import nltk
+import json
+import random
 import pickle
 
 from bs4 import BeautifulSoup
@@ -9,23 +11,46 @@ from cogroo_interface import Cogroo
 
 TESTE_DIR = 'CORPUS/TESTE/'
 TREINO_DIR = 'CORPUS/TREINO/'
-ARQUIVOS = ['CORPUS ESPACO DO TRABALHADOR.txt', ]
-# 'CORPUS ESPORTES.txt', 'CORPUS POLICIA.txt', 'CORPUS SEU PROBLEMA E NOSSO.txt']
+ARQUIVOS = ['CORPUS ESPACO DO TRABALHADOR.txt',
+            'CORPUS ESPORTES.txt', 'CORPUS POLICIA.txt', 'CORPUS SEU PROBLEMA E NOSSO.txt']
 
 
 class LimparArquivosTexto:
     def __init__(self):
         self.generate_structured_files(ARQUIVOS)
-        cogroo = Cogroo.Instance()
 
     def generate_structured_files(self, arquivos):
         for filename in arquivos:
             with open('CORPUS/{}'.format(filename), 'r') as arq:
                 lines = arq.readlines()
-                texto = ' '.join(lines)
                 textos = self.split_texts(lines)
                 textos = self.clean_texts(textos)
-                # self.create_bag_of_words(texto=texto)
+
+                len_size_treino = len(textos) * 0.8
+
+                textos_treino = self.get_texts(textos, 0, int(len_size_treino))
+                textos_teste = self.get_texts(textos, int(len_size_treino), len(textos))
+
+                bow_treino = self.create_bag_of_words(textos_treino)
+                bow_teste = self.create_bag_of_words(textos_teste)
+
+                with open('CORPUS/TESTE/{}'.format(filename), 'w') as new_arq:
+                    to_json = json.dumps(textos_teste, ensure_ascii=False, indent=2)
+                    new_arq.write(to_json)
+
+                with open('CORPUS/TREINO/{}'.format(filename), 'w') as new_arq:
+                    to_json = json.dumps(textos_treino, ensure_ascii=False, indent=2)
+                    new_arq.write(to_json)
+
+                with open('CORPUS/TESTE/BoW/{}'.format(filename), 'w') as new_arq:
+                    words = [(k, bow_teste[k]) for k in sorted(bow_teste, key=bow_teste.get, reverse=True)]
+                    for k, v in words:
+                        new_arq.write("{} - {}\n".format(k, v))
+
+                with open('CORPUS/TREINO/BoW/{}'.format(filename), 'w') as new_arq:
+                    words = [(k, bow_treino[k]) for k in sorted(bow_treino, key=bow_treino.get, reverse=True)]
+                    for k, v in words:
+                        new_arq.write("{} - {}\n".format(k, v))
 
     def split_texts(self, lines):
         textos = {}
@@ -40,10 +65,70 @@ class LimparArquivosTexto:
         return textos
 
     def clean_texts(self, textos):
-        for texto in textos:
-            t = textos[texto]
-
+        for titulo in textos:
+            texto = textos[titulo]
+            texto = " ".join(texto)
+            bes = BeautifulSoup(texto, "html.parser")
+            texto = bes.get_text()  # Remove as Tags do texto
+            texto = self.remove_emails(texto)  # Remover os e-mails
+            texto = self.lemmatize(texto)
+            tags = self.pos_annotation(texto)
+            unigram, bigrams, trigrams = self.create_ngrams(tags)
+            textos[titulo] = {'texto': texto, 'pos': tags, 'ngrams': {'unigram': unigram,
+                              'bigrams': bigrams, 'trigrams': trigrams}}
         return textos
+
+    def create_ngrams(self, tags):
+        unigram = []
+        bigrams = []
+        trigrams = []
+
+        # Unigram
+        for words in tags:
+            tmp_words = []
+            for w in words:
+                if w[1] == 'VERB' or w[1] == 'ADJ' or w[1] == 'ADV' or w[1] == 'NOUN':
+                    tmp_words.append(w[0])
+            for u in nltk.ngrams(tmp_words, 1):
+                unigram.append(u)
+
+        # Bigrams
+        for words in tags:
+            tmp_words = []
+            for w in words:
+                if w[1] == 'VERB' or w[1] == 'ADP' or w[1] == 'ADJ' or w[1] == 'ADV' or w[1] == 'NOUN':
+                    tmp_words.append(w[0])
+            for u in nltk.ngrams(tmp_words, 2):
+                bigrams.append(u)
+
+        # Trigrams
+        for words in tags:
+            tmp_words = []
+            for w in words:
+                if w[1] == 'VERB' or w[1] == 'ADP' or w[1] == 'ADJ' or w[1] == 'ADV' or w[1] == 'NOUN':
+                    tmp_words.append(w[0])
+            for u in nltk.ngrams(tmp_words, 3):
+                trigrams.append(u)
+
+        return unigram, bigrams, trigrams
+
+    def get_random_texts(self, texts, percent):
+        # Necessita de melhorias
+        len_size = len(texts) * percent
+        new_texts = {}
+        for i in range(0, int(len_size)):
+            text = random.choice(list(texts.keys()))
+            if text not in new_texts:
+                new_texts[text] = texts[text]
+        return new_texts
+
+    def get_texts(self, texts, start, end):
+        new_texts = {}
+        keys = list(texts.keys())[start:end]
+        for key in keys:
+            new_texts[key] = texts[key]
+
+        return new_texts
 
     def remove_emails(self, text):
         re_email = r'\S*@\S*\s?'
@@ -54,26 +139,52 @@ class LimparArquivosTexto:
     def remove_stopwords(self, text):
         stopwords = set(nltk.corpus.stopwords.words('portuguese'))
         words = [word for word in text if word not in stopwords]
-        return words
+        return " ".join(words)
 
     def pos_annotation(self, text):
-        tagger = pickle.load(open("tagger.pkl"))
+        tagger = pickle.load(open("tagger.pkl", "rb"))
         portuguese_sent_tokenizer = nltk.data.load("tokenizers/punkt/portuguese.pickle")
         sentences = portuguese_sent_tokenizer.tokenize(text)
         tags = [tagger.tag(nltk.word_tokenize(sentence)) for sentence in sentences]
+        # tags = [tagger.tag(sentence.split()) for sentence in sentences]
         return tags
 
     def lemmatize(self, text):
-        pass
+        cogroo = Cogroo.Instance()
+        words = []
+        texto = nltk.wordpunct_tokenize(text)
+        # texto = text.split()
+        for t in texto:
+            t_aux = cogroo.lemmatize(t)
+            words.append(t_aux)
+        return " ".join(words)
 
-    def create_bag_of_words(self, texto):
-        bes = BeautifulSoup(texto, "html.parser")  # Removendo as marcações
-        texto = bes.get_text()  # Remove as Tags do texto
-        texto = self.remove_emails(texto)  # Remover os e-mails
-        texto = self.remove_stopwords(texto)  # Remover as stopwords
-        texto = nltk.wordpunct_tokenize(texto)  # Tokenizar as palavras
-        # pos_texto = self.pos_annotation(texto)  # Adição da notação POS (part of speech)
-        texto = nltk.Text(texto)  # Texto nos padrões do NLTK
+    def create_bag_of_words(self, textos):
+        termos = dict()
+        for texto in textos:
+            for i in textos[texto]['ngrams']['unigram']:
+                term = " ".join(i)
+                term = term.lower()
+                if term in termos:
+                    termos[term] += 1
+                else:
+                    termos[term] = 1
+            for i in textos[texto]['ngrams']['bigrams']:
+                term = " ".join(i)
+                term = term.lower()
+                if term in termos:
+                    termos[term] += 1
+                else:
+                    termos[term] = 1
+
+            for i in textos[texto]['ngrams']['trigrams']:
+                term = " ".join(i)
+                term = term.lower()
+                if term in termos:
+                    termos[term] += 1
+                else:
+                    termos[term] = 1
+        return termos
 
 
 if __name__ == "__main__":
